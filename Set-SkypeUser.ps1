50b81f3b7e978761e0610efe49660b0641a91319
spmiddlebrooks <spmiddlebrooks@gmail.com>
2017-09-19 09:38:31 -0400

    Update Set-SkypeUser.ps1

--- Set-SkypeUser.ps1
+++ Set-SkypeUser.ps1
@@ -4,14 +4,14 @@
 .PARAMETER
 .EXAMPLE
 .NOTES
-	Version: 1.0
-	Updated: 9/15/2017 1814
+	Version: 1.1
+	Updated: 9/18/2017 1546
 	Original Author: Scott Middlebrooks (Git Hub: spmiddlebrooks)
 .LINK
 	https://github.com/spmiddlebrooks
 #>
 #Requires -Version 3.0
-#Requires -Modules Lync
+
 
 [CmdletBinding(SupportsShouldProcess=$true)]
 param( 
@@ -22,9 +22,51 @@ param(
 		})]	
 		[string] $FilePath,
 	[Parameter(Mandatory=$False)]
+		[ValidateScript({
+			if ( (Test-Path (split-path $_ -Parent)) ) {$True}
+			else {Throw "LogPath $_ not found"}
+		})]	
+		[string] $LogPath="",
+	[Parameter(Mandatory=$False)]
 		[switch] $ShowCommands
 )
 
+function Set-ModuleStatus { 
+<#
+.SYNOPSIS
+.DESCRIPTION
+.PARAMETER
+.EXAMPLE
+.NOTES
+	Original Author: Pat Richard
+.LINK
+	https://www.ucunleashed.com/938
+#>
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param	(
+		[parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Mandatory = $true, HelpMessage = "No module name specified!")] 
+		[ValidateNotNullOrEmpty()]
+		[string] $name
+	)
+	PROCESS{
+		# Executes once for each pipeline object
+		# the $_ variable represents the current input object		
+		if (!(Get-Module -name "$name")) { 
+			if (Get-Module -ListAvailable | Where-Object Name -eq "$name") { 
+				Import-Module -Name "$name"
+				# module was imported
+				return $true
+			} else {
+				# module was not available
+				return $false
+			}
+		} else {
+			# Write-Output "$_ module already imported"
+			return $true
+		} 
+	} # end PROCESS
+} 
+# End function Set-ModuleStatus
 
 function Test-CsvFormat {
     <#
@@ -33,8 +75,8 @@ function Test-CsvFormat {
     .PARAMETER
     .EXAMPLE
     .NOTES
-	    Version: 1.0
-	    Updated: 9/15/2017 0753
+	    Version: 1.1
+	    Updated: 9/16/2017
 	    Original Author: Scott Middlebrooks (Git Hub: spmiddlebrooks)
     .LINK
 	    https://github.com/spmiddlebrooks
@@ -42,29 +84,55 @@ function Test-CsvFormat {
 	param (
 		[string] $CsvFilePath
 	)
+    $ColumnsCsvContainsRequired = $True
+    $ColumnsCsvContainsTargets = $False
+
 	$Csv = Import-CSV $CsvFilePath
 
-	## List all columns that MUST be in the csv:
-	$ColumnsExpected = @(
+
+	## These columns are required be in the CSV:
+	$ColumnsRequired = @(
 		'userPrincipalName'
 	)
 
-	## Verify that all expected columns are there (additional columns in the csv will be ignored)
-	$ColumnsOK = $True
+    # These columns are the valid operation Target columns, at least 1 must be present in the CSV, otherwise we have nothing to do
+    $ValidTargetColumns = @('TargetCsEnabled','TargetSipAddress','TargetRegistrarPool','TargetLineUri','TargetEnterpriseVoiceEnabled')
+    $ValidTargetColumns += @(Get-Command -Module $UcPlatform | ForEach { if ($_.Name -match $GrantCsRegex) {'Target' + $matches[1]} })
+    
+
+	## Get a list of all the column names in the CSV
 	$ColumnsCsv = $Csv | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
 	
-	$ColumnsExpected | ForEach-Object {
+    # Check the list of CSV columns for all the required columns
+	$ColumnsRequired | ForEach-Object {
 		If ($ColumnsCsv -notcontains $_) {
-			$ColumnsOK = $False
-			"Expected column not found: '$($_)'" | Write-Host -ForegroundColor Red
+			$ColumnsCsvContainsRequired = $False
+			"Required column not found: '$($_)'" | Write-Host -ForegroundColor Magenta
 		}
 	}
-	
-	If ($ColumnsOK) {
+
+    # Check the list of CSV columns for the operation Target columns
+	$ValidTargetColumns | ForEach-Object {
+		If ($ColumnsCsv -contains $_) {
+			$ColumnsCsvContainsTargets = $true
+		}
+	}
+
+	# If all required columns and at least 1 operation Target column are present, we are good to go
+	If ($ColumnsCsvContainsRequired -and $ColumnsCsvContainsTargets) {
 		return $Csv,$ColumnsCsv
 	}
-	else {
-		Throw "The csv format is incorrect!"
+    # Stop the script and throw an exception if we are missing the required columns
+	elseif ($ColumnsCsvContainsRequired -eq $False -and $ColumnsCsvContainsTargets) {
+		Throw "The CSV does not contain the required columns"
+	}
+    # Stop the script and throw an exception if don't have any operation Targets
+	elseif ($ColumnsCsvContainsRequired -and $ColumnsCsvContainsTargets -eq $False) {
+		Throw "The CSV does not contain any Target columns"
+	}
+    # Stop the script and throw an exception if we are missing the required columns and operation Target columns
+    elseif ($ColumnsCsvContainsRequired -eq $False -and $ColumnsCsvContainsTargets -eq $False) {
+		Throw "The CSV does not contain the required columns NOR does it contain any Target columns"
 	}
 }
 # End function Test-CsvFormat
@@ -76,8 +144,8 @@ function Invoke-CommandLine {
     .PARAMETER
     .EXAMPLE
     .NOTES
-	    Version: 1.0
-	    Updated: 9/15/2017 0753
+	    Version: 1.1
+	    Updated: 9/18/2017 1546
 	    Original Author: Scott Middlebrooks (Git Hub: spmiddlebrooks)
     .LINK
 	    https://github.com/spmiddlebrooks
@@ -89,56 +157,104 @@ function Invoke-CommandLine {
 	)
     # $PSCmdlet.ShouldProcess("Target", "Action")
 	if ( $PSCmdlet.ShouldProcess($userPrincipalName,$Command) ) {
-		if ($ShowCommands) { Write-Color -Text 'Show Command: ', $Command -Color Cyan, Green }
-		Invoke-Expression $Command
+		try {
+            Invoke-Expression $Command
+		    if ($ShowCommands) {
+                Write-Log -Text 'Command Successful ', $Command -Color Green, Cyan -LogPath $LogPath
+            }
+            else {
+                Write-Log -NoConsole -Text 'Command Successful ', $Command -LogPath $LogPath
+            }
+        }
+        catch {
+            Write-Log -Text 'Command Failed ', "$Command ", $($_.Exception.Message) -Color Red, Cyan, Magenta -LogPath $LogPath
+        }
 	}
 }
 # End function Invoke-CommandLine
 
-function Write-Color {
+function Write-Log {
     <#
     .SYNOPSIS
     .DESCRIPTION
     .PARAMETER
     .EXAMPLE
     .NOTES
-	    Version: 1.0
-	    Updated: 9/15/2017 1041
+	    Version: 1.1
+	    Updated: 9/18/2017 1546
 	    Original Author: Scott Middlebrooks (Git Hub: spmiddlebrooks)
     .LINK
 	    https://github.com/spmiddlebrooks
     #>
 	param (
-		[String[]] $Text, 
+		[string[]] $Text, 
+        [switch] $NoLog = $false,
+        [switch] $NoConsole = $false,
 		[ConsoleColor[]] $Color = "White", 
-		[int] $StartTab = 0, 
-		[int] $LinesBefore = 0,
-		[int] $LinesAfter = 0,
-		[string] $LogFile = "",
-		$TimeFormat = "yyyy-MM-dd HH:mm:ss"
+        #[int] $StartTab = 0, 
+		#[int] $LinesBefore = 0,
+		#[int] $LinesAfter = 0,
+		[string] $LogPath = "",
+        $TimeFormat = "s"
+		#$TimeFormat = "yyyy-MM-dd HH:mm:ss"
 	)
     # [enum]::GetValues([System.ConsoleColor]) | Foreach-Object {Write-Host $_ -ForegroundColor $_ }
  
+    $DateTime = Get-Date -Format $TimeFormat
     $DefaultColor = $Color[0]
-    if ($LinesBefore -ne 0) {  for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host "`n" -NoNewline } } # Add empty line before
-    if ($StartTab -ne 0) {  for ($i = 0; $i -lt $StartTab; $i++) { Write-Host "`t" -NoNewLine } }  # Add TABS before text
-    if ($Color.Count -ge $Text.Count) {
-        for ($i = 0; $i -lt $Text.Length; $i++) { Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine } 
-    } else {
-        for ($i = 0; $i -lt $Color.Length ; $i++) { Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine }
-        for ($i = $Color.Length; $i -lt $Text.Length; $i++) { Write-Host $Text[$i] -ForegroundColor $DefaultColor -NoNewLine }
-    }
-    Write-Host
-    if ($LinesAfter -ne 0) {  for ($i = 0; $i -lt $LinesAfter; $i++) { Write-Host "`n" } }  # Add empty line after
-    if ($LogFile -ne "") {
-        $TextToFile = ""
-        for ($i = 0; $i -lt $Text.Length; $i++) {
-            $TextToFile += $Text[$i]
+
+    <#
+     # Add empty line before
+    if ($LinesBefore -ne 0) {
+        for ($i = 0; $i -lt $LinesBefore; $i++) {
+            Write-Host "`n" -NoNewline
         }
-        Write-Output "[$([datetime]::Now.ToString($TimeFormat))]$TextToFile" | Out-File $LogFile -Encoding unicode -Append
+    }
+
+    # Add TABS before text
+    if ($StartTab -ne 0) {
+        for ($i = 0; $i -lt $StartTab; $i++) {
+            Write-Host "`t" -NoNewLine
+        }
+    }
+    #>
+
+    if ($NoConsole -eq $False) {
+        if ($Color.Count -ge $Text.Count) {
+            for ($i = 0; $i -lt $Text.Length; $i++) {
+                Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine
+            }
+        } 
+        else {
+            for ($i = 0; $i -lt $Color.Length ; $i++) {
+                Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine
+            }
+            for ($i = $Color.Length; $i -lt $Text.Length; $i++) {
+                Write-Host $Text[$i] -ForegroundColor $DefaultColor -NoNewLine
+            }
+        }
+    }
+
+    #Write-Host
+
+    <#
+    # Add empty line after
+    if ($LinesAfter -ne 0) {
+        for ($i = 0; $i -lt $LinesAfter; $i++) {
+            Write-Host "`n"
+        }
+    }
+    #>
+
+    if ($NoLog -eq $False -and $LogPath -ne "") {
+        $TextToFile = @($DateTime) + $Text
+        #for ($i = 0; $i -lt $Text.Length; $i++) {
+        #    $TextToFile += $Text[$i]
+        #}
+        Write-Output ($TextToFile -join "`t") | Out-File $LogPath -Encoding ASCII -Append
     }
 }
-# End function Start-Command
+# End function Write-Log
 
 
 ############################################################################
@@ -147,20 +263,43 @@ $E164RegEx = '^tel:\+\d{7,15}(;ext=\d+)?$'
 $SipUriRegex = '^sip:.+@.+$'
 $NonSipUriRegex = '^.+@.+$'
 $CsPolicyNameRegEx = '^[A-Za-z0-9\-_ ]+$'
+$GrantCsRegex = 'Target(DialPlan|\w+Policy)'
 
+# Attempt to load the SkypeforBusiness module
+if (Set-ModuleStatus SkypeForBusiness) {
+    $UcPlatform = 'SkypeforBusiness'
+}
+# If the SkypeforBusiness module is not present, attempt to load the Lync module
+elseif (Set-ModuleStatus Lync) {
+    $UcPlatform = 'Lync'
+}
+# If neither module is present throw an exception to prevent further operations
+else {
+    throw "Cannot proceed, could not load Skype for Business or Lync PowerShell module"
+}
+
+# Load the CSV file retrieving the list of users $AllCsvUsers and a list of columns $ColumnsCsv
+If ( $AllCsvUsers,$ColumnsCsv = Test-CsvFormat $FilePath ) {
 	
-If ($AllCsvUsers,$ColumnsCsv = Test-CsvFormat $FilePath) {
-	
+    # Iterate through each row in the list of users from th
 	Foreach ($CsvUser in $AllCsvUsers) {
-    
-		if ($AllCsvUsers.Count -le 1) { $AllCsvUsersCount = 1 }
-		else { $AllCsvUsersCount = $AllCsvUsers.Count }
+    [bool] $EnableCsUserSuccess=$false
+
+		if ($AllCsvUsers.Count -le 1) {
+            $AllCsvUsersCount = 1 
+        }
+		else {
+            $AllCsvUsersCount = $AllCsvUsers.Count
+        }
 	
 		Write-Progress -Activity "Processing Users" -Status "Processing $RowNumber of $AllCsvUsersCount" -PercentComplete (($RowNumber / $AllCsvUsersCount) * 100)
+        Start-Sleep -Milliseconds 200
 		$RowNumber += 1
 
 		# Check to see if user is already enabled in Lync/Skype
-		$CsUserObject = Get-CsUser -Identity $($CsvUser.userPrincipalName) -ErrorAction SilentlyContinue
+        if ($($CsvUser.userPrincipalName)) {
+		    $CsUserObject = Get-CsUser -Identity $($CsvUser.userPrincipalName) -ErrorAction SilentlyContinue
+        }
 
 		#### Logic for when a user is already Lync/Skype enabled
         # If a user is already enabled in Lync/Skype...
@@ -198,13 +337,19 @@ If ($AllCsvUsers,$ColumnsCsv = Test-CsvFormat $FilePath) {
 		elseif ( $($CsvUser.TargetCsEnabled) -eq $True -and -Not $CsUserObject ) {
             # Enable the user
                 if ( $($CsvUser.TargetSipAddress) -match $SipUriRegex ) {
-                    Invoke-CommandLine $CsvUser.userPrincipalName "Enable-CsUser -Identity $($CsvUser.userPrincipalName) -RegistrarPool $($CsvUser.TargetRegistrarPool) -SipAddress $($CsvUser.TargetSipAddress)"
+                    if (Invoke-CommandLine $CsvUser.userPrincipalName "Enable-CsUser -Identity $($CsvUser.userPrincipalName) -RegistrarPool $($CsvUser.TargetRegistrarPool) -SipAddress $($CsvUser.TargetSipAddress)") {
+                        $EnableCsUserSuccess=$true
+                    }
                 }
                 elseif ( $($CsvUser.TargetSipAddress) -match $NonSipUriRegex ) {
-                    Invoke-CommandLine $CsvUser.userPrincipalName "Enable-CsUser -Identity $($CsvUser.userPrincipalName) -RegistrarPool $($CsvUser.TargetRegistrarPool) -SipAddress sip:$($CsvUser.TargetSipAddress)"
+                    if (Invoke-CommandLine $CsvUser.userPrincipalName "Enable-CsUser -Identity $($CsvUser.userPrincipalName) -RegistrarPool $($CsvUser.TargetRegistrarPool) -SipAddress sip:$($CsvUser.TargetSipAddress)") {
+                        $EnableCsUserSuccess=$true
+                    }
                 }
             # Wait for the changes to replicate
-			Start-Sleep -Seconds 10
+			if ( $EnableCsUserSuccess ) {
+                Start-Sleep -Seconds 10
+            }
             # If TargetEnterpriseVoiceEnabled is True and TargetLineUri matches E164 formatting, EV enable the user and set LineUri
 			if ( $($CsvUser.TargetEnterpriseVoiceEnabled) -eq $True -and $($CsvUser.TargetLineUri) -match $E164RegEx ) {
                     Invoke-CommandLine $CsvUser.userPrincipalName "Set-CsUser -Identity $($CsvUser.userPrincipalName) -EnterpriseVoiceEnabled `$True -LineUri $($CsvUser.TargetLineUri)"
@@ -212,7 +357,8 @@ If ($AllCsvUsers,$ColumnsCsv = Test-CsvFormat $FilePath) {
 		}
 
 		foreach ($Column in $ColumnsCsv) {
-			if ( ( $CsUserObject -or $($CsvUser.TargetCsEnabled) ) -and $Column -match 'Target(DialPlan|\w+Policy)' ) {
+			                        #$($CsvUser.TargetCsEnabled)
+            if ( ($CsUserObject -or $EnableCsUserSuccess) -and $Column -match $GrantCsRegex ) {
 				$Command = $null
 				$GrantCsCommand = "Grant-Cs" + $Matches[1]
 
